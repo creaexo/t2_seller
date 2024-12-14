aa6912aef3ee69766e81b0d4abb6aa7856a2a909
creaexo <creaexo@gmail.com>
2024-12-15 00:05:05 +0500

    refactor: Функция получения заголовков теперь возвращает более реалистичное значение.
    Функции взаимодействия с T2 возвращают словари. Код авторизации теперь берётся из переменной окружения, а если её нет, запрашивается при старте.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import os
 import secrets
 from datetime import datetime, timezone, timedelta
 from enum import StrEnum
 import requests
 
-AUTH_CODE = str(input())
+AUTH_CODE = os.environ.get('T2_AUTH_CODE') or str(input('Код авторизации: '))
 
 MIN_UNIT_COST = 0.8
+SEC_CH_UA = '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"'
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 
 
 class TrafficType(StrEnum):
@@ -38,6 +41,7 @@ def create_order(
     cost: int | None,
     staff: Stuff,
 ):
+    refer = 'my'
     if not cost:
         cost = value * MIN_UNIT_COST
     json = {
@@ -51,12 +55,11 @@ def create_order(
         },
         "trafficType": "voice"
     }
-    r = requests.put(
+    return requests.put(
         f'https://ekt.t2.ru/api/subscribers/{number}/exchange/lots/created',
         json=json,
-        headers=get_headers(auth_code, 5, '', '')
-    )
-    return r.json()
+        headers=get_headers(auth_code, 5, SEC_CH_UA, UA, refer)
+    ).json()
 
 
 def update_order(
@@ -65,8 +68,9 @@ def update_order(
     auth_code: str,
     value: int,
     cost: int | None,
-    emoji: list[str | None]
+    emoji: list[Emoji | None],
 ):
+    refer = 'my'
     if not cost:
         cost = value * MIN_UNIT_COST
     json = {
@@ -77,12 +81,11 @@ def update_order(
             "currency": "rub"
         }
     }
-    r = requests.patch(
+    return requests.patch(
         f'https://ekt.t2.ru/api/subscribers/{number}/exchange/lots/created/{order_id}',
         json=json,
-        headers=get_headers(auth_code, 5, '', '')
-    )
-    return r.json()
+        headers=get_headers(auth_code, 5, SEC_CH_UA, UA, refer)
+    ).json()
 
 
 def get_local_time(time_zone: int):
@@ -90,14 +93,17 @@ def get_local_time(time_zone: int):
     return time.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + time.strftime('%z')
 
 
-def get_headers(auth_code, time_zone: int, ua: str, browser: str):
+def get_headers(auth_code, time_zone: int, sec_ch_ua: str, ua: str, refer: str):
     return {
         'Accept': 'application/json, text/plain, */*',
+        'accept-encoding': 'zstd',
+        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
         'Authorization': auth_code,
-
-        'Content-type': 'application/json',
-        'referer': 'https://ekt.t2.ru/stock-exchange/my',
-        'sec-ch-ua': browser,
+        'cache-control': 'no-cache',
+        'pragma': 'no-cache',
+        'priority': 'u=1, i',
+        'referer': f'https://ekt.t2.ru/stock-exchange/{refer}',
+        'sec-ch-ua': sec_ch_ua,
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': 'Windows',
         'tele2-user-agent': 'web',
